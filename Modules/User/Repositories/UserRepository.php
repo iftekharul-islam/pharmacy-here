bd527add530af35e4698f0f21ee36f41cf6a2fc7
Md Feroj Bepari <feroj21@gmail.com>
2020-07-07 17:50:10 +0600

    User module design

---
+++ Modules/User/Repositories/UserRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\User\Repositories;
+
+
+use Modules\User\Models\User;
+
+class UserRepository {
+
+	
+	public function all() {
+		return User::all();
+	}
+	
+	public function get($id) {
+	
+	}
+	
+	public function create() {
+		
+	}
+	
+	public function delete() {
+		
+	}
+	
+	public function update() {
+		
+	}
+}
---
+++ Modules/User/readme.md
@@ -0,0 +1,3 @@
+**Instructions**
+
+Please remove the users migration from the root migraton folder if you use this module.
