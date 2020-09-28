07c398ae2405b2be201841f5bcc1e167de7368d3
iftekharul <iftekharul@augnitive.com>
2020-09-28 12:18:09 +0600

    SSL commerce Testing

--- routes/web.php
+++ routes/web.php
@@ -59,4 +59,18 @@ Route::group(['prefix' => 'cart'], function () {
     Route::delete('remove-from-cart', 'CartController@remove');
 });
 
+// SSLCOMMERZ Start
+Route::get('/example1', 'SslCommerzPaymentController@exampleEasyCheckout');
+Route::get('/example2', 'SslCommerzPaymentController@exampleHostedCheckout');
+
+Route::post('/pay', 'SslCommerzPaymentController@index');
+Route::post('/pay-via-ajax', 'SslCommerzPaymentController@payViaAjax');
+
+Route::post('/success', 'SslCommerzPaymentController@success');
+Route::post('/fail', 'SslCommerzPaymentController@fail');
+Route::post('/cancel', 'SslCommerzPaymentController@cancel');
+
+Route::post('/ipn', 'SslCommerzPaymentController@ipn');
+//SSLCOMMERZ END
+
 
