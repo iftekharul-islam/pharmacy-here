149c6b6f72e730a28de31ba9437f9ba4d0a8dbd9
iftekharul <iftekharul@augnitive.com>
2020-10-06 16:52:01 +0600

    Search image and home page ui fix

--- resources/views/order/show.blade.php
+++ resources/views/order/show.blade.php
@@ -90,21 +90,3 @@
         </div>
     </div>
 @endsection
-
-
-@section('js')
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <!-- font awesome -->
-    <script src="{{ asset('js/icon.js') }}"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <!-- font awesome -->
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <!-- owl -->
-    <script src="js/owl.carousel.min.js"></script>
-    <!-- custom jquery -->
-    <script src="js/main.js"></script>
-
-@endsection
