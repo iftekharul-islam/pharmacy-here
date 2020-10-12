8910a9fb8ab53e40064e33a017d5f3ec7c801e56
Payel Das <payel@augnitive.com>
2020-10-12 14:49:16 +0600

    Initiated earn points

---
+++ Modules/Points/Database/Migrations/2020_10_12_073251_create_refers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRefersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('refers', function (Blueprint $table) {
+            $table->id();
+
+            $table->unsignedBigInteger('referred_by');
+            $table->unsignedBigInteger('referred_to');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('refers');
+    }
+}
