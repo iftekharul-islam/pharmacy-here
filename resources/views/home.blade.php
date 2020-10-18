00e3368e37cce1105a0e08daf800f3c1d523eedb
iftekharul <iftekharul@augnitive.com>
2020-10-19 00:06:47 +0600

    Search result make scrollable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,8 @@
             background: white;
             border-bottom-left-radius: 15px;
             border-bottom-right-radius: 15px;
+            max-height:300px;
+            overflow-y:auto;
 
         }
         #searchResult li {
