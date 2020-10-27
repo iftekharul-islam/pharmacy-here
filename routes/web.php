209a2d7dd607cbf52d5b4a8eb327d38b815f7df9
iftekharul <iftekharul@augnitive.com>
2020-10-27 18:47:33 +0600

    Hot fix

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,7 @@ Route::get('/locale/{locale}', 'LocalizationController');
 
 
 // Basic pages
-Route::get('/', function (){
-    return 'hello';
-})->name('home');
-
-//Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 Route::get('/terms-of-use', 'HomeController@terms')->name('terms');
 Route::get('/faq', 'HomeController@faq')->name('faq');
 Route::get('/return-&-refund-page', 'HomeController@refundPage')->name('return.page');
