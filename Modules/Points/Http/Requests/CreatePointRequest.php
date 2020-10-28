bfe9c321ddd27d9076d7d3e697413667b7676ce0
Payel Das <payel@augnitive.com>
2020-10-28 19:00:27 +0600

    Manual point store to a customer from admin panel

---
+++ Modules/Points/Http/Requests/CreatePointRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Points\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePointRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'customer_id' => 'required',
+            'point' => 'required|min:0',
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+}
